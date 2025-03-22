acd3d5e614226336c7f2b970a241419583d0d5bb
Lorenzo Bertetto <lorenzo.bertetto@gmail.com>
2025-03-22 11:14:56 +0100

    Catppuccin colorscheme adjustments to color diffview and fidget.

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -2,4 +2,12 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
+  ---@module "catppuccin"
+  ---@type CatppuccinOptions
+  opts = {
+    integrations = {
+      fidget = true,
+      diffview = true,
+    },
+  },
 }
