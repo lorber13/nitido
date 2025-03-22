b4871861169ede6be69fd83373ee27d6a90f3750
Lorenzo Bertetto <lorenzo.bertetto@gmail.com>
2025-03-22 11:24:12 +0100

    Added telescope and search keymap.

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  -- or                              , branch = '0.1.x',
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    {
+      "<leader>s",
+      "<cmd>Telescope live_grep<cr>",
+      desc = "Global [S]earch",
+    },
+  },
+}
